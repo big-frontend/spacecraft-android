8d248b023d3682335fb5bc85ba1b4b74c11d92d8
hawks.jamesf <hawks.jamesf@gmail.com>
2018-09-24 18:48:20 +0800

    Android gradle dsl

--- app/src/main/java/com/hawksjamesf/simpleweather/SimpleWeatherApplication.java
+++ app/src/main/java/com/hawksjamesf/simpleweather/SimpleWeatherApplication.java
@@ -1,6 +1,8 @@
 package com.hawksjamesf.simpleweather;
 
-import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+import android.support.multidex.MultiDexApplication;
 
 import com.orhanobut.logger.AndroidLogAdapter;
 import com.orhanobut.logger.Logger;
@@ -11,17 +13,26 @@ import com.tencent.bugly.crashreport.CrashReport;
  * Copyright ® $ 2017
  * All right reserved.
  * Code Link : https://github.com/HawksJamesf/SimpleWeather
- *  @author: hawks jamesf
- *  @since: 2017/7/4
+ *
+ * @author: hawks jamesf
+ * @since: 2017/7/4
  */
 
-public class SimpleWeatherApplication extends Application {
-    private static final String TAG ="SimpleWeatherApp---";
-    private  static AppComponent appComponent;
+public class SimpleWeatherApplication extends MultiDexApplication {
+    private static final String TAG = "SimpleWeatherApp---";
+    private static AppComponent appComponent;
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
-        Logger.addLogAdapter(new AndroidLogAdapter(PrettyFormatStrategy.newBuilder().tag(TAG).build()){
+        Logger.addLogAdapter(new AndroidLogAdapter(PrettyFormatStrategy.newBuilder().tag(TAG).build()) {
             @Override
             public boolean isLoggable(int priority, String tag) {
                 return BuildConfig.DEBUG;
@@ -32,6 +43,7 @@ public class SimpleWeatherApplication extends Application {
 
         CrashReport.initCrashReport(getApplicationContext(), "a5f4e5063e", false);
     }
+
     public static AppComponent getAppComponent() {
         return appComponent;
     }
