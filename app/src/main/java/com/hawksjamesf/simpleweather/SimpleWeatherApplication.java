879706aa56f2c1d94dbfddf87535781412398dc3
hawks jamesf <hawks.jamesf@gmail.com>
2018-09-25 09:56:48 +0800

    Revert "Android gradle dsl"
    This reverts commit 8d248b023d3682335fb5bc85ba1b4b74c11d92d8.

--- app/src/main/java/com/hawksjamesf/simpleweather/SimpleWeatherApplication.java
+++ app/src/main/java/com/hawksjamesf/simpleweather/SimpleWeatherApplication.java
@@ -1,8 +1,6 @@
 package com.hawksjamesf.simpleweather;
 
-import android.content.Context;
-import android.support.multidex.MultiDex;
-import android.support.multidex.MultiDexApplication;
+import android.app.Application;
 
 import com.orhanobut.logger.AndroidLogAdapter;
 import com.orhanobut.logger.Logger;
@@ -13,26 +11,17 @@ import com.tencent.bugly.crashreport.CrashReport;
  * Copyright ® $ 2017
  * All right reserved.
  * Code Link : https://github.com/HawksJamesf/SimpleWeather
- *
- * @author: hawks jamesf
- * @since: 2017/7/4
+ *  @author: hawks jamesf
+ *  @since: 2017/7/4
  */
 
-public class SimpleWeatherApplication extends MultiDexApplication {
-    private static final String TAG = "SimpleWeatherApp---";
-    private static AppComponent appComponent;
-
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-        MultiDex.install(this);
-
-    }
-
+public class SimpleWeatherApplication extends Application {
+    private static final String TAG ="SimpleWeatherApp---";
+    private  static AppComponent appComponent;
     @Override
     public void onCreate() {
         super.onCreate();
-        Logger.addLogAdapter(new AndroidLogAdapter(PrettyFormatStrategy.newBuilder().tag(TAG).build()) {
+        Logger.addLogAdapter(new AndroidLogAdapter(PrettyFormatStrategy.newBuilder().tag(TAG).build()){
             @Override
             public boolean isLoggable(int priority, String tag) {
                 return BuildConfig.DEBUG;
@@ -43,7 +32,6 @@ public class SimpleWeatherApplication extends MultiDexApplication {
 
         CrashReport.initCrashReport(getApplicationContext(), "a5f4e5063e", false);
     }
-
     public static AppComponent getAppComponent() {
         return appComponent;
     }
