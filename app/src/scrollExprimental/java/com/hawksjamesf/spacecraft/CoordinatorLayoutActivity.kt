e95ae2e7b5256cbfbc7437194361bc80118d7a28
hawks.jamesf <hawks.jamesf@gmail.com>
2019-05-26 13:48:07 +0800

    feat—>嵌套滚动v2

---
+++ app/src/scrollExprimental/java/com/hawksjamesf/spacecraft/CoordinatorLayoutActivity.kt
@@ -0,0 +1,62 @@
+package com.hawksjamesf.spacecraft
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager.widget.PagerAdapter
+import kotlinx.android.synthetic.main.activity_coordinatorlayout.*
+
+/**
+ * Copyright ® $ 2019
+ * All right reserved.
+ *
+ * @author: hawksjamesf
+ * @email: hawksjamesf@gmail.com
+ * @since: Feb/16/2019  Sat
+ */
+class CoordinatorLayoutActivity : Activity() {
+    val mContentAdapter = OuterAdapter()
+    var dataList: MutableList<Int> = object : ArrayList<Int>() {
+        init {
+            for (i in 0..30) {
+                add(i)
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coordinatorlayout)
+
+        val mRvContent = findViewById<RecyclerView>(R.id.rv_content)
+        mRvContent.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+        mRvContent.adapter = mContentAdapter
+        val pagerSnapHelper = PagerSnapHelper()
+        pagerSnapHelper.attachToRecyclerView(mRvContent)
+
+        vp_content.adapter = ProxyAdapter()
+    }
+
+    inner class ProxyAdapter : PagerAdapter() {
+        override fun isViewFromObject(view: View, `object`: Any): Boolean {
+            return view == `object`
+        }
+
+        override fun getCount(): Int = mContentAdapter.itemCount
+        override fun instantiateItem(container: ViewGroup, position: Int): Any {
+            val viewHolder = mContentAdapter.onCreateViewHolder(container, position)
+            mContentAdapter.onBindViewHolder(viewHolder, position)
+            container.addView(viewHolder.itemView)
+            return viewHolder.itemView
+        }
+
+        override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+            container.removeView(`object` as View?)
+        }
+    }
+
+}
---
+++ app/src/scrollExprimental/java/com/hawksjamesf/spacecraft/listener/NestedScrollingBehavior.java
@@ -0,0 +1,19 @@
+package com.hawksjamesf.spacecraft.listener;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Copyright ® $ 2019
+ * All right reserved.
+ *
+ * @author: hawksjamesf
+ * @email: hawksjamesf@gmail.com
+ * @since: May/25/2019  Sat
+ */
+public class NestedScrollingBehavior extends ViewOffsetBehavior<View> {
+    public NestedScrollingBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
