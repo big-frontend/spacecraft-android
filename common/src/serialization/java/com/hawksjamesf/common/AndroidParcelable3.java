d52fd192436937d8f329847676fccdd1152c5650
hawks.jamesf <hawks.jamesf@gmail.com>
2019-08-03 08:58:44 +0800

    feat--->common:序列化

---
+++ common/src/serialization/java/com/hawksjamesf/common/AndroidParcelable3.java
@@ -0,0 +1,14 @@
+package com.hawksjamesf.common;
+
+import org.parceler.Parcel;
+
+/**
+ * Copyright ® $ 2017
+ * All right reserved.
+ *
+ * @author: hawks.jamesf
+ * @since: Aug/03/2019  Sat
+ */
+@Parcel(Parcel.Serialization.BEAN)
+public class AndroidParcelable3 {
+}
