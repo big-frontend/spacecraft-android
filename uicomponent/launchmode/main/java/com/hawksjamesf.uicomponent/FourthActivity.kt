d7c18e7242e2011d5323877a053e8cdaf23946dd
hawks.jamesf <hawks.jamesf@gmail.com>
2019-11-14 17:29:01 +0800

    启动模式

---
+++ uicomponent/launchmode/main/java/com/hawksjamesf.uicomponent/FourthActivity.kt
@@ -0,0 +1,44 @@
+package com.hawksjamesf.uicomponent
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+
+class FourthActivity : AppCompatActivity() {
+    companion object{
+        @JvmStatic
+        fun startActivity(context: Context){
+            val intent = Intent(context,FourthActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            context.startActivity(intent)
+        }
+        const val TAG="FourthActivity"
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        Log.d(TAG,"onNewIntent:${intent?.flags}")
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val frameLayout = FrameLayout(this)
+        frameLayout.setBackgroundColor(Color.CYAN)
+        val bt = Button(this)
+        bt.text = "fourth"
+        bt.isAllCaps = false
+        bt.setOnClickListener {
+            val intent= Intent(this@FourthActivity,ThirdActivity::class.java)
+//            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            this@FourthActivity.startActivity(intent)
+        }
+        frameLayout.addView(bt,ViewGroup.LayoutParams.MATCH_PARENT,200)
+        val lp = FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT)
+        setContentView(frameLayout, lp)
+    }
+}
