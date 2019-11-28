be7b27bda0eadd0a576012e198009b98eb13659e
hawks.jamesf <hawks.jamesf@gmail.com>
2019-11-28 13:32:17 +0800

    add something

---
+++ network/printlogs/main/java/com/hawksjamesf/network/LogActivity.kt
@@ -0,0 +1,14 @@
+package com.hawksjamesf.network
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Copyright ® $ 2019
+ * All right reserved.
+ *
+ * @author: hawksjamesf
+ * @email: hawksjamesf@gmail.com
+ * @since: Nov/28/2019  Thu
+ */
+class LogActivity: AppCompatActivity() {
+}
