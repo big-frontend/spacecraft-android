c10039035689f38690903c71cf66cbf335872450
hawks.jamesf <hawks.jamesf@gmail.com>
2019-12-03 22:27:34 +0800

    长图

---
+++ uicomponent/photo/main/java/com/hawksjamesf/uicomponent/LargePhotoActivity.kt
@@ -0,0 +1,19 @@
+package com.hawksjamesf.uicomponent
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Copyright ® $ 2017
+ * All right reserved.
+ *
+ * @author: hawks.jamesf
+ * @since: Dec/03/2019  Tue
+ */
+class LargePhotoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+    }
+}
