d8aa4651ea90f2db0579c91a707aa18d83761d17
hawks.jamesf <hawks.jamesf@gmail.com>
2019-12-03 23:33:13 +0800

    长图

--- uicomponent/photo/main/java/com/hawksjamesf/uicomponent/LargePhotoActivity.kt
+++ uicomponent/photo/main/java/com/hawksjamesf/uicomponent/LargePhotoActivity.kt
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity
 class LargePhotoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
-
+        val photoview= PhotoView(this)
+        setContentView(photoview)
     }
 }
---
+++ uicomponent/photo/main/java/com/hawksjamesf/uicomponent/PhotoView.kt
@@ -0,0 +1,24 @@
+package com.hawksjamesf.uicomponent
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+
+
+/**
+ * Copyright ® $ 2017
+ * All right reserved.
+ *
+ * @author: hawks.jamesf
+ * @since: Dec/03/2019  Tue
+ */
+class PhotoView: AppCompatImageView {
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    init {
+
+    }
+
+
+}
