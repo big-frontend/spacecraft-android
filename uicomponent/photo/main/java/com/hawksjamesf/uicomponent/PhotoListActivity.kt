962201e30987e5f0b6b9287d65ca3086f8349527
hawks.jamesf <hawks.jamesf@gmail.com>
2019-12-07 00:06:20 +0800

    add something

--- uicomponent/photo/main/java/com/hawksjamesf/uicomponent/PhotoListActivity.kt
+++ uicomponent/photo/main/java/com/hawksjamesf/uicomponent/PhotoListActivity.kt
@@ -1,7 +1,9 @@
 package com.hawksjamesf.uicomponent
 
+import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.ViewGroup
 import android.widget.HorizontalScrollView
@@ -20,8 +22,13 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.ListPreloader
+import com.bumptech.glide.RequestBuilder
+import com.bumptech.glide.integration.recyclerview.RecyclerViewPreloader
+import com.bumptech.glide.util.ViewPreloadSizeProvider
 import com.hawksjamesf.uicomponent.repository.CacheRegion
 import com.hawksjamesf.uicomponent.repository.ServiceLocator
+import java.util.*
 
 class PhotoListActivity : AppCompatActivity() {
 
@@ -54,8 +61,17 @@ class PhotoListActivity : AppCompatActivity() {
         rvPhotoList.setBackgroundColor(Color.CYAN)
         rvPhotoList.layoutManager = LinearLayoutManager(this)
         rvPhotoList.addItemDecoration(Divider(this))
-        val adapter = PhotoListAdapter()
+
+
+        val adapter = PhotoListAdapter(this)
         rvPhotoList.adapter = adapter
+        val preloader = RecyclerViewPreloader(adapter.glideRequest, adapter, adapter.preloadSizeProvider, 4)
+        rvPhotoList.addOnScrollListener(preloader)
+        rvPhotoList.setRecyclerListener { holder ->
+            (holder.itemView as HorizontalScrollView).children.forEach {
+                adapter.glideRequest.clear(it)
+            }
+        }
         setContentView(rvPhotoList)
 
         //init data
@@ -74,10 +90,20 @@ class PhotoListActivity : AppCompatActivity() {
         viewModel.itemList?.observe(this, observer)
     }
 
-    inner class PhotoListAdapter : PagedListAdapter<String, RecyclerView.ViewHolder>(diffCallback) {
+    inner class PhotoListAdapter(val context: Context) : PagedListAdapter<String, RecyclerView.ViewHolder>(diffCallback), ListPreloader.PreloadModelProvider<String> {
+
+        val glideRequest = Glide.with(context)
+        val requestBuilder = glideRequest.asDrawable()
+        val preloadSizeProvider = ViewPreloadSizeProvider<String>()
+
+        init {
+
+
+        }
 
         val thumbnailBitmapList = arrayListOf<Bitmap>()
         val urlList = arrayListOf<String>()
+
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
             val scrollView = HorizontalScrollView(parent.context)
             val linearLayout = LinearLayout(parent.context)
@@ -99,9 +125,14 @@ class PhotoListActivity : AppCompatActivity() {
             for (index in 0 until itemCount) {
                 val imageView = linearLayout.getChildAt(index) as? ImageView
                 imageView ?: continue
-                Glide.with(imageView.context)
-                        .load(viewModel.itemList?.value?.get(index))
+//                Glide.with(imageView.context)
+//                        .load(viewModel.itemList?.value?.get(index))
+//                        .into(imageView)
+                requestBuilder.load(holder.itemView.tag as String?)
                         .into(imageView)
+                        .clearOnDetach()
+                preloadSizeProvider.setView(imageView)
+
                 viewModel.itemList?.value?.get(index)?.let { urlList.add(it) }
 
                 imageView.setOnClickListener {
@@ -112,11 +143,15 @@ class PhotoListActivity : AppCompatActivity() {
                     PhotoActivity.startActivity(this@PhotoListActivity, thumbnailBitmapList, urlList, imageView.tag as Int)
                 }
             }
+
         }
 
         override fun onCurrentListChanged(previousList: PagedList<String>?, currentList: PagedList<String>?) {
             super.onCurrentListChanged(previousList, currentList)
         }
+
+        override fun getPreloadItems(position: Int): MutableList<String> = Collections.singletonList(urlList[position])
+        override fun getPreloadRequestBuilder(item: String): RequestBuilder<Drawable>? = requestBuilder.load(item)
     }
 
 }
