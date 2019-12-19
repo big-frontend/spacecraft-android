95e4eedf129b33888f1b29dafb5e88814c0a573d
hawks.jamesf <hawks.jamesf@gmail.com>
2019-12-19 23:30:56 +0800

    ndk

---
+++ common/ypose/main/jni/hawks.h
@@ -0,0 +1,16 @@
+//
+// Created by hawks.jamesf on 12/19/19.
+//
+
+#ifndef SPACECRAFTANDROID_HAWKS_H
+#define SPACECRAFTANDROID_HAWKS_H
+
+
+
+class hawks {
+
+};
+
+
+
+#endif //SPACECRAFTANDROID_HAWKS_H
