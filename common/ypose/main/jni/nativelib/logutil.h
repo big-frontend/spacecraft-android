d28b532268ceb8a8ee66bcaeb9d14d8135f1887c
hawks.jamesf <hawks.jamesf@gmail.com>
2019-12-23 23:25:02 +0800

    add something

---
+++ common/ypose/main/jni/nativelib/logutil.h
@@ -0,0 +1,4 @@
+//
+// Created by hawks.jamesf on 12/23/19.
+//
+#include <android/log.h>
--- common/ypose/main/jni/nativelib/native-lib.cpp
+++ common/ypose/main/jni/nativelib/native-lib.cpp
@@ -6,9 +6,14 @@
 #include <pthread.h>
 #include <android/log.h>
 #include <assert.h>
+#include <time.h>
+
+
 extern "C" JNIEXPORT jstring JNICALL Java_com_hawksjamesf_common_YPoseActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+    struct  timeval Time;
+
     return env->NewStringUTF(hello.c_str());
 }
