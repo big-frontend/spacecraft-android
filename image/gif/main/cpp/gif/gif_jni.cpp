69677c2aabbeb36190fbe3e793ac83d84d42b707
hawks.jamesf <hawks.jamesf@gmail.com>
2020-01-05 23:58:30 +0800

    add something

--- image/gif/main/cpp/gif/gif_jni.cpp
+++ image/gif/main/cpp/gif/gif_jni.cpp
@@ -35,12 +35,78 @@ Java_com_hawksjamesf_image_GifImageView_setSource(JNIEnv *env, jobject gifImageV
     GifCodec *gifCodec = new GifCodecFromAssets(assetName, assetManager);
     gifCodec->fileName;
     GifFileType *gifFileType = gifCodec->decodingGif();
-    LOGD(MODULE_NAME, "filename: %s, fileLength %d bytes", assetName, gifCodec->getFileSize());
-    LOGD(MODULE_NAME, "screen-->width: %d,height: %d", gifFileType->SWidth, gifFileType->SHeight);
+    LOGD(MODULE_NAME, "file name: %s, file size: %d bytes", assetName, gifCodec->getFileSize());
+    LOGD(MODULE_NAME,
+         "width: %d,height: %d,left %d,top:%d,right:%d,bottom:%d \ncolor resloution: %d, background color: %d,AspectByte %d",
+         gifFileType->SWidth, gifFileType->SHeight, gifFileType->Image.Left, gifFileType->Image.Top,
+         gifFileType->Image.Width, gifFileType->Image.Height, gifFileType->SColorResolution,
+         gifFileType->SBackGroundColor,
+         gifFileType->AspectByte);
+
+    DGifSlurp(gifFileType);
+    LOGD(MODULE_NAME,
+         "width: %d,height: %d,left %d,top:%d,right:%d,bottom:%d \ncolor resloution: %d, background color: %d,AspectByte %d",
+         gifFileType->SWidth, gifFileType->SHeight, gifFileType->Image.Left, gifFileType->Image.Top,
+         gifFileType->Image.Width, gifFileType->Image.Height, gifFileType->SColorResolution,
+         gifFileType->SBackGroundColor,
+         gifFileType->AspectByte);
+
+//    for (int i = 0; i < gifFileType->SColorResolution; ++i) {
+//        for (int j = 0; j < gifFileType->SColorMap[i].ColorCount; ++j) {
+//            LOGD(MODULE_NAME,"SColorMap--->i/j: %d/%d,ColorCount: %d,BitsPerPixel: %d,SortFlag: %d, rgb: %d %d %d",
+//                 i, j,
+//                 gifFileType->SColorMap[i].ColorCount,
+//                 gifFileType->SColorMap[i].BitsPerPixel,
+//                 gifFileType->SColorMap[i].SortFlag,
+//                 gifFileType->SColorMap[i].Colors[j].Red,
+//                 gifFileType->SColorMap[i].Colors[j].Green,
+//                 gifFileType->SColorMap[i].Colors[j].Blue
+//            );
+//        }
+//    }
+    for (int i = 0; i < gifFileType->ImageCount; ++i) {
+        for (int j = 0; j < gifFileType->SavedImages[i].ExtensionBlockCount; ++j) {
+            LOGD(MODULE_NAME,
+                 "SavedImages--->i/j: %d/%d, left: %d ,top: %d ,right %d,bottom: %d,\nExtensionBlockCount: %d,ByteCount : %d ,Bytes: %d,Function: %d",
+                 i, j,
+                 gifFileType->SavedImages[i].ImageDesc.Left,
+                 gifFileType->SavedImages[i].ImageDesc.Top,
+                 gifFileType->SavedImages[i].ImageDesc.Width,
+                 gifFileType->SavedImages[i].ImageDesc.Height,
+                 gifFileType->SavedImages[i].ExtensionBlockCount,
+                 gifFileType->SavedImages[i].ExtensionBlocks[j].ByteCount,
+                 gifFileType->SavedImages[i].ExtensionBlocks[j].Bytes,
+                 gifFileType->SavedImages[i].ExtensionBlocks[j].Function
+            );
+        }
+
+    }
+    for (int i = 0; i < gifFileType->SColorMap->ColorCount; ++i) {
+        LOGD(MODULE_NAME, "SColorMap--->index: %d,color rgb: %d , %d , %d",
+             i,
+             gifFileType->SColorMap->Colors[i].Red,
+             gifFileType->SColorMap->Colors[i].Green,
+             gifFileType->SColorMap->Colors[i].Blue
+        );
+    }
+
+    for (int i = 0; i < gifFileType->ExtensionBlockCount; ++i) {
+        LOGD(MODULE_NAME, "ExtensionBlock--->index: %d,ByteCount : %d ,Bytes: %d,Function: %d ",
+             i,
+             gifFileType->ExtensionBlocks[i].ByteCount,
+             gifFileType->ExtensionBlocks[i].Bytes,
+             gifFileType->ExtensionBlocks[i].Function
+        );
+    }
+
+
+
 //    off_t start = 0, length = 0;
 //    int fd = AAsset_openFileDescriptor(aAsset, &start, &length);
 //    lseek(fd, start, SEEK_CUR);
 //    decodingGif(fd, false);
+    int error = -1;
+    DGifCloseFile(gifFileType, &error);
     delete gifCodec;
 }
 /**
@@ -52,7 +118,7 @@ Java_com_hawksjamesf_image_GifImageView_setSource1(JNIEnv *env, jobject gifImage
     char *uriPath = const_cast<char *>(env->GetStringUTFChars(uriPathFromJava, 0));
 //    GifCodec *gifCodec = new GifCodec(uriPath);
 //    GifFileType *gifFileType = gifCodec->decodingGif();
-//    LOGD(MODULE_NAME, "filename: %s, fileLength %d bytes", uriPath,
+//    LOGD(MODULE_NAME, "file name: %s, file size %d bytes", uriPath,
 //         gifFileType->ExtensionBlocks->ByteCount);
 //    LOGD(MODULE_NAME, "screen-->width: %d,height: %d", gifFileType->SWidth, gifFileType->SHeight);
 }
