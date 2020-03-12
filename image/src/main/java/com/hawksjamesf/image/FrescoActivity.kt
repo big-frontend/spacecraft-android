9deba094c15dd942fe53fa296647509868c5e934
HawksJamesf <Hawks.Jamesf@gmail.com>
2020-03-12 21:58:57 +0800

    add something
    Signed-off-by: HawksJamesf <Hawks.Jamesf@gmail.com>

---
+++ image/src/main/java/com/hawksjamesf/image/FrescoActivity.kt
@@ -0,0 +1,28 @@
+package com.hawksjamesf.image
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+
+/**
+ * Copyright ® $ 2017
+ * All right reserved.
+ *
+ * @author: hawks.jamesf
+ * @since: Dec/10/2019  Tue
+ */
+class FrescoActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val imageView = ImageView(this)
+        imageView.scaleType = ImageView.ScaleType.MATRIX
+        imageView.setBackgroundColor(Color.BLUE)
+        val lp = ViewGroup.LayoutParams(40, 40)
+        setContentView(imageView, lp)
+    }
+
+}
