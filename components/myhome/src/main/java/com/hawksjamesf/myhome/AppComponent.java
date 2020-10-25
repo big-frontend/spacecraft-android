f5200aef20fc88b5ac2e1d964c5034942394df2e
jamesfchen <hawks.jamesf@gmail.com>
2020-10-25 16:28:23 +0800

    [bug] fix bug
    Signed-off-by: jamesfchen <hawks.jamesf@gmail.com>

--- components/myhome/src/main/java/com/hawksjamesf/myhome/AppComponent.java
+++ components/myhome/src/main/java/com/hawksjamesf/myhome/AppComponent.java
@@ -1,33 +1,33 @@
-package com.hawksjamesf.myhome;
-
-import com.hawksjamesf.network.NetComponent;
-import com.hawksjamesf.myhome.scopes.UserScope;
-import com.hawksjamesf.myhome.ui.SplashActivity;
-import com.hawksjamesf.myhome.ui.home.HomeActivity;
-import com.hawksjamesf.signin.Client;
-import com.hawksjamesf.signin.SigInModule;
-import com.hawksjamesf.signin.SignInActivity;
-
-import dagger.Component;
-
-/**
- * Copyright ® $ 2017
- * All right reserved.
- * Code Link : https://github.com/HawksJamesf/Spacecraft
- *
- * @author: hawks jamesf
- * @since: 2017/7/4
- */
-@UserScope
-@Component(dependencies = NetComponent.class, modules = {AppModule.class, SigInModule.class}/*more modules*/)
-public interface AppComponent {
-    void inject(SplashActivity splashActivity);
-
-    void inject(HomeActivity homeActivity);
-
-    Client client();
-
-    void inject(SignInActivity signInActivity);
-
-
-}
+//package com.hawksjamesf.myhome;
+//
+//import com.hawksjamesf.network.NetComponent;
+//import com.hawksjamesf.myhome.scopes.UserScope;
+//import com.hawksjamesf.myhome.ui.SplashActivity;
+//import com.hawksjamesf.myhome.ui.home.HomeActivity;
+//import com.hawksjamesf.signin.Client;
+//import com.hawksjamesf.signin.SigInModule;
+//import com.hawksjamesf.signin.SignInActivity;
+//
+//import dagger.Component;
+//
+///**
+// * Copyright ® $ 2017
+// * All right reserved.
+// * Code Link : https://github.com/HawksJamesf/Spacecraft
+// *
+// * @author: hawks jamesf
+// * @since: 2017/7/4
+// */
+//@UserScope
+//@Component(dependencies = NetComponent.class, modules = {AppModule.class, SigInModule.class}/*more modules*/)
+//public interface AppComponent {
+//    void inject(SplashActivity splashActivity);
+//
+//    void inject(HomeActivity homeActivity);
+//
+//    Client client();
+//
+//    void inject(SignInActivity signInActivity);
+//
+//
+//}
--- components/myhome/src/main/java/com/hawksjamesf/myhome/AppModule.java
+++ components/myhome/src/main/java/com/hawksjamesf/myhome/AppModule.java
@@ -1,12 +1,8 @@
 package com.hawksjamesf.myhome;
 
 import com.hawksjamesf.loader.App;
-import com.hawksjamesf.myhome.scopes.UserScope;
-import com.hawksjamesf.signin.Client;
-import com.hawksjamesf.network.source.SignInDataSource;
 
 import dagger.Module;
-import dagger.Provides;
 
 /**
  * Copyright ® $ 2017
@@ -25,12 +21,12 @@ public class AppModule {
         this.app = app;
     }
 
-    @Provides
-    @UserScope
-    public Client provideClient(SignInDataSource dataSource) {
-        return new Client(dataSource);
-
-    }
+//    @Provides
+//    @UserScope
+//    public Client provideClient(SignInDataSource dataSource) {
+//        return new Client(dataSource);
+//
+//    }
 
 //    @Provides
 //    @UserScope
