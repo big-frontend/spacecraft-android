13113efbcadc80931a59a7826b56ed287636ac9c
jamesfchen <hawks.jamesf@gmail.com>
2020-11-20 23:12:39 +0800

    [feat] add config
    Signed-off-by: jamesfchen <hawks.jamesf@gmail.com>

--- yposed/src/main/java/com/hawksjamesf/yposed/YPosedActivity.kt
+++ yposed/src/main/java/com/hawksjamesf/yposed/YPosedActivity.kt
@@ -1,9 +1,16 @@
 package com.hawksjamesf.yposed
 
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.annotation.Keep
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_yposed.*
+import java.lang.StringBuilder
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import java.util.*
 
 class YPosedActivity : AppCompatActivity() {
 
@@ -13,6 +20,7 @@ class YPosedActivity : AppCompatActivity() {
         bt_hook_frida.setOnClickListener {
             bt_hook_frida.text = stringFromJNI()
         }
+        Log.d("cjf", "YPosedActivity sha1:" + sHA1(this))
 
     }
 
@@ -24,7 +32,32 @@ class YPosedActivity : AppCompatActivity() {
 
         init {
             System.loadLibrary("hawks")
-            System.loadLibrary("frida-gadget")
+            System.loadLibrary("gadget")
         }
+    }
+
+    fun sHA1(context: Context): String? {
+        try {
+            val info = context.packageManager.getPackageInfo(context.packageName, PackageManager.GET_SIGNATURES)
+            Log.d("cjf", "YPosedActivity firstInstallTime:${info.firstInstallTime} lastUpdateTime:${info.lastUpdateTime}")
+            val cert = info.signatures[0].toByteArray()
+            val md = MessageDigest.getInstance("SHA1")
+            val publicKey = md.digest(cert)
+            val hexString = StringBuffer()
+            for (i in publicKey.indices) {
+                val appendString = Integer.toHexString(0xFF and publicKey[i].toInt())
+                        .toUpperCase(Locale.US)
+                if (appendString.length == 1) hexString.append("0")
+                hexString.append(appendString)
+                hexString.append(":")
+            }
+            val result = hexString.toString()
+            return result.substring(0, result.length - 1)
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        } catch (e: NoSuchAlgorithmException) {
+            e.printStackTrace()
+        }
+        return null
     }
 }
