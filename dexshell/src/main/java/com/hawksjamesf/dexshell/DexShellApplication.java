ea39154d2f7b1052cea6fbf67ce21c2d15b2fc21
jamesfchen <hawks.jamesf@gmail.com>
2020-11-22 21:44:49 +0800

    [feat] init dex shell project
    Signed-off-by: jamesfchen <hawks.jamesf@gmail.com>

---
+++ dexshell/src/main/java/com/hawksjamesf/dexshell/DexShellApplication.java
@@ -0,0 +1,23 @@
+package com.hawksjamesf.dexshell;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Copyright ® $ 2017
+ * All right reserved.
+ *
+ * @author: hawks.jamesf
+ * @since: Nov/22/2020  Sun
+ */
+class DexShellApplication  extends Application {
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ script/dex_protector.py
@@ -0,0 +1,7 @@
+"""
+[Android应用加固原理](https://juejin.cn/post/6844903952345989134#heading-12)
+[Android DEX加固方案与原理](https://blog.csdn.net/EthanCo/article/details/102867224)
+[性能优化 (七) APK 加固之 Dex 加解密，反编译都看不到项目主要代码。](https://juejin.cn/post/6844903858582323208)
+"""
+
+raw_apk_path='/Users/hawks.jamesf/tech/Spacecraft/SpacecraftAndroid/app/build/outputs/apk/release/app-arm64-v8a-release.apk'
