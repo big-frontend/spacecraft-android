77563e898d47005638316c5f7e0e400b054c232d
hawks.jamesf <hawks.jamesf@gmail.com>
2020-12-17 13:35:28 +0800

    add trace time to Splash page

--- components/myhome/src/main/java/com/hawksjamesf/myhome/ui/SplashActivity.java
+++ components/myhome/src/main/java/com/hawksjamesf/myhome/ui/SplashActivity.java
@@ -15,6 +15,7 @@ import com.google.firebase.perf.metrics.AddTrace;
 import com.google.firebase.perf.metrics.HttpMetric;
 import com.google.firebase.perf.metrics.Trace;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.hawksjamesf.annotations.TraceTime;
 import com.hawksjamesf.common.util.ActivityUtil;
 import com.hawksjamesf.loader.App;
 import com.hawksjamesf.myhome.R;
@@ -48,7 +49,7 @@ public class SplashActivity extends AbsPermissionsActivity {
         setContentView(R.layout.activity_splash);
 
     }
-//    @TraceTime
+    @TraceTime
     protected void onRequestPermissionsResult() {
         Observable.timer(1, TimeUnit.SECONDS)
                 .observeOn(AndroidSchedulers.mainThread())
