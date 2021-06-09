199f7012b371a389ab68c13ab93d727cb122c072
jamesfchen <hawksjamesf@gmail.com>
2021-06-09 20:03:33 +0800

    add

---
+++ components/myhome/src/main/java/com/jamesfchen/myhome/dangrous/Test1.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * asfdasfasdfasdf
+ */
+package com.jamesfchen.myhome.dangrous;
+
+import android.graphics.Color;
+
+
+public class Test1 {
+    void test(){
+        Integer.parseInt("1000");
+        Integer.valueOf("1000");
+//        Long.valueOf("111");
+//        Long.parseLong("111");
+        Color.parseColor("#1232424");
+    }
+}
