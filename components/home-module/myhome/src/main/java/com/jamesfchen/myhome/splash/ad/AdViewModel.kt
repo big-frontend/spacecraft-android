c67a528a33cd4dca54a4647465bce5ec0ba306b2
hawks.jamesf <hawks.jamesf@gmail.com>
2021-09-02 17:55:12 +0800

    add param

--- components/home-module/myhome/src/main/java/com/jamesfchen/myhome/splash/ad/AdViewModel.kt
+++ components/home-module/myhome/src/main/java/com/jamesfchen/myhome/splash/ad/AdViewModel.kt
@@ -1,7 +1,10 @@
 package com.jamesfchen.myhome.splash.ad
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
-class AdViewModel : ViewModel() {
+class AdViewModel(app: Application, savedStateHandle: SavedStateHandle) : AndroidViewModel(app) {
     // TODO: Implement the ViewModel
 }
