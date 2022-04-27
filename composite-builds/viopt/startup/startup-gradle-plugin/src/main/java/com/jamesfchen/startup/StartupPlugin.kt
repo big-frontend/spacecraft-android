59d6d4dfd10b31597d0ff9a472d94fdda1525e9b
jamesfchen <hawksjamesf@gmail.com>
2022-04-27 20:11:49 +0800

    update startup plugin

--- composite-builds/viopt/startup/startup-gradle-plugin/src/main/java/com/jamesfchen/startup/StartupPlugin.kt
+++ composite-builds/viopt/startup/startup-gradle-plugin/src/main/java/com/jamesfchen/startup/StartupPlugin.kt
@@ -1,16 +1,26 @@
 package com.jamesfchen.startup
 
+import com.android.build.api.transform.Format
+import com.android.build.api.transform.QualifiedContent
 import com.android.build.api.transform.TransformInvocation
+import com.android.build.gradle.internal.pipeline.TransformManager
+import com.google.common.collect.ImmutableSet
 import com.jamesfchen.ClassInfo
 import com.jamesfchen.ScanClassPlugin
+import java.io.File
 
 class StartupPlugin : ScanClassPlugin() {
-    override fun getName() = "startup"
-
+    override fun getName() = "Startup"
+    lateinit var dest: File
     override fun onScanBegin(transformInvocation: TransformInvocation) {
+        dest = transformInvocation.outputProvider.getContentLocation(
+            "Startup", TransformManager.CONTENT_CLASS,
+            ImmutableSet.of(QualifiedContent.Scope.PROJECT), Format.DIRECTORY
+        )
     }
 
     override fun onScanClass(info: ClassInfo) {
+
     }
 
     override fun onScanEnd() {
