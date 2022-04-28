af303a9d99236011ca0ae38217c7078711b868f6
jamesfchen <hawksjamesf@gmail.com>
2022-04-28 15:59:25 +0800

    update

--- composite-builds/viopt/startup/startup-api/src/main/java/com/jamesfchen/startup/annotation.kt
+++ composite-builds/viopt/startup/startup-api/src/main/java/com/jamesfchen/startup/annotation.kt
@@ -1,5 +1,12 @@
 package com.jamesfchen.startup
 
+/**
+ * job
+ * - attach在哪个进程
+ * - 立即进行 or 延迟执行 or 根据条件执行 or MQ idle执行
+ * - 启动优先级
+ * - 依赖模块
+ */
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.BINARY)
 annotation class Job() {}
