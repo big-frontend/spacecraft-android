4522c06f4ffbbb6e2a43ee970ef3cebdbb17514b
JamesfChen <hawks.jamesf@gmail.com>
2022-05-25 23:50:57 +0800

    创建task list composer lib
    Signed-off-by: JamesfChen <hawks.jamesf@gmail.com>

--- composite-builds/settings.gradle.kts
+++ composite-builds/settings.gradle.kts
@@ -2,5 +2,6 @@ include(
     ":viapm:viapm-annotations", ":viapm:viapm-processor", ":viapm:viapm-gradle-plugin",":viapm:viapm-api",
     ":viopt:startup:startup-gradle-plugin",":viopt:startup:startup-api",
     ":kcp",
-    ":proguard-plugin"
+    ":proguard-plugin",
+    ":tasklist-composer"
 )
