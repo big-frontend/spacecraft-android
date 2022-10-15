bf7897fb3f95d4de38a8210376441942e89f070a
JamesfChen <hawks.jamesf@gmail.com>
2022-10-15 12:55:01 +0800

    add super-retrofit
    Signed-off-by: JamesfChen <hawks.jamesf@gmail.com>

---
+++ android/framework/base/src/main/java/retrofit2/callFactory/NoContentResponseBody.java
@@ -0,0 +1,41 @@
+package retrofit2.callFactory;
+
+import javax.annotation.Nullable;
+
+import okhttp3.MediaType;
+import okhttp3.ResponseBody;
+import okio.BufferedSource;
+
+/**
+ * Copyright ® $ 2021
+ * All right reserved.
+ *
+ * @author jamesfchen
+ * @email hawksjamesf@gmail.com
+ * @since 9月/28/2021  周二
+ */
+public final class NoContentResponseBody extends ResponseBody {
+    private final @Nullable
+    MediaType contentType;
+    private final long contentLength;
+
+    public NoContentResponseBody(@Nullable MediaType contentType, long contentLength) {
+        this.contentType = contentType;
+        this.contentLength = contentLength;
+    }
+
+    @Override
+    public MediaType contentType() {
+        return contentType;
+    }
+
+    @Override
+    public long contentLength() {
+        return contentLength;
+    }
+
+    @Override
+    public BufferedSource source() {
+        throw new IllegalStateException("Cannot read raw response body of a converted body.");
+    }
+}
